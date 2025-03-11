a168ff9d4a038f5c2de65f3c9165c32a3afb8344
Laura-th1 <laut15@outlook.com>
2025-03-10 20:18:51 -0500

    actualización de 10 de marzo de 2025 2000hs

--- view_plataforma/update_mision.php
+++ view_plataforma/update_mision.php
@@ -12,7 +12,7 @@
             flex-direction: column;
             align-items: center;
         }
-        #contenedor, #formulario{
+        #contenedor, .formulario{
             display:flex;
             flex-direction: column;
             align-items: center;
@@ -21,15 +21,16 @@
             aspect-ratio: 1/1;
             background-color:#7A1F1F;
         }
-        #formulario{
+        #formulario, #formulario1{
             width: 80%;
+            aspect-ratio: 4/1;
         }
         textarea{
             width: 90%;
             aspect-ratio: 1/1;
             font-size: italic;
         }
-        #boton-mision{
+        .form-botones{
             background: linear-gradient(to bottom, #ffd700, white);
             box-shadow: inset 2px 2px 5px rgba(0, 0, 0, 0.3), 0 0 5px rgba(0, 120, 215, 0.5); 
             border-radius: 5px;
@@ -37,6 +38,7 @@
             font-size: 1.2vw;
             font-weight: bold;
             color:#4A0D0D;
+            width:30%;
             margin:2%;
         }
         h2{
@@ -50,12 +52,30 @@
 <body>
     <div id="contenedor">
         <h2>Actualizace la Misión:</h2>
-        <form action="index.php?action=update_mision" method="post" id="formulario">
-            <textarea name="nueva_mision" placeholder="Actualice aquí la misión">
+        <form action="index.php?action=update_mision" method="post" class="formulario" id="formulario"onsubmit="validarFormulario(event)">
+            <textarea name="nueva_mision" placeholder="Actualice aquí la misión" id="text_mision">
             </textarea>
-            <input type="submit" value="Actualizar Misión" id="boton-mision">
+            <input type="submit" value="Actualizar Misión" class="form-botones">
 
         </form>
+        <form action="index.php?action=club_principal" method="get" class="formulario" id="formulario1" >
+                    <button type="submit" name="action" value="principal" class="form-botones">Vista Principal</button>
+        </form>
     </div>
+    <script>
+    function validarFormulario(event) {
+      const textarea = document.getElementById('text_mision');
+      const texto = textarea.value.trim();
+
+      // Contar las palabras, dividiendo el texto por los espacios
+      const palabras = texto.split(/\s+/).filter(Boolean); // filter(Boolean) elimina los elementos vacíos
+
+      // Verificar si hay al menos 3 palabras
+      if (palabras.length < 20) {
+        alert('Por favor, escribe al menos 20 palabras.');
+        event.preventDefault(); // Evitar que se envíe el formulario
+      }
+    }
+  </script>
 </body>
 </html>
--- view_plataforma/ver_vision.php
+++ view_plataforma/ver_vision.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Misión de La Liga Tolimense de Lucha</title>
     <style>
+         @font-face {
+  font-family: 'fuente2';
+  src: url('fonts/fuente2.ttf') format('truetype');/*Nombre real : struck */
+}
+
         body{
             width: 100%;
             height: 100%;
@@ -22,16 +27,37 @@
 
         }
         p{
-            font-family: italic;
+            font-family: 'fuente3';
             font-size: 1.3vw;
             padding: 5%;
             color:white;
+            font-style:italic;
+            letter-spacing: 3px;
         }
         h2{
-            font-family: italic;
+            font-family: 'fuente3';
             font-size:1.5vw;
             margin:5%;
             color:white;
+            font-style:italic;
+            letter-spacing: 4px;
+        }
+        #form_boton{
+            display: flex;
+            width: 100%;
+            align-items: center;
+            justify-content: center;
+           
+        }
+        .botones{
+            background: linear-gradient(to bottom, #ffd700, white);
+            box-shadow: inset 2px 2px 5px rgba(0, 0, 0, 0.3), 0 0 5px rgba(0, 120, 215, 0.5); 
+            border-radius: 5px;
+            font-family:italic;
+            font-size: 1.2vw;
+            font-weight: bold;
+            color:#4A0D0D;
+            margin:4%;
         }
     </style>
 </head>
@@ -43,5 +69,8 @@
         echo "<p>". $vision['vision'] ."<p>";
         ?>
     </p>
+    <form action="index.php?action=club_principal" method="get" class="form-botones" id="form_boton">
+                    <button type="submit" name="action" value="principal" class="botones">Vista Principal</button>
+    </form>
 
     </div>
