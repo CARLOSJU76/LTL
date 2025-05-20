861c4af80b9fbf90b3b436a39ea893f10dbf21b9
CAJUGATO76 <cajugato76@hotmail.com>
2025-05-20 13:39:25 -0500

    actualización de 200525 1400 hs

--- view_sesiones/list_sesionBySite.php
+++ view_sesiones/list_sesionBySite.php
@@ -1,129 +1,17 @@
-<style>
-    @font-face {
-  font-family: 'fuente3';
-  src: url('fonts/fuente3.ttf') format('truetype');/*Nombre real : struck */
-}
-
-    body{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        font-family: Arial, Helvetica, sans-serif;
-        font-style: italic;
-        color:white;
-    }
-    #menu{
-        display: flex;
-        flex-direction: row;
-        width:80%;
-        justify-content: space-around;
-        padding:0;
-        border: solid gold 1px;
-        border-radius: 3px;
-        margin-top: 3%;
-    }
-    .form-insert{
-        width: 90%;
-        display:flex;
-        flex-direction: column;
-        align-items: center;
-        background-color: #4A0D0D;
-        border: solid gold 2px;
-        margin-bottom: 3%;
-        padding:2%;
-    }
-    #uno,#dos,#tres{
-        width: 33;
-        margin-top: 3%;
-        margin-bottom: 3%;
-      
-    }
-    input{
-        width: 80%;
-       font-size: 1.3vw;
-       font-style: italic;
-       padding:2%;
-    }
-    option{
-        font-size: 0.8vw;
-        font-style: italic;
-    }
-    select{
-        font-size: 1vw;
-        font-style:italic;
-        padding: 1%;
-        width: 40%;
-    }
-    .form-botones{
-
-           
-            background-color: transparent; 
-            border:  solid gold 1px;
-            border-radius: 5px;
-            font-family:Arial, Helvetica, sans-serif;
-            font-style: italic;
-            font-size: 1vw;
-            font-weight: bold;
-            color:gold;
-            width:40%;
-            margin:2%;
-        }
-    td, th{
-        padding: 1%;
-        font-size:1vw;
-        border:solid gold 1px;
-
-
-    }
-    .lugarE{
-        width: 55%;
-    }
-    table{    
-        width: 100%;
-        border: solid gold 1px;
-    }
-    #formulario{
-        margin:5%;
-    }
-    .lugar{
-        width: 40%;
-    }
-    .boton-sub{
-        width: 98%;
-        background-color: transparent;
-        border: solid gold 0.1rem;
-        font-size:0.8rem;
-        border-radius: 3px;
-        margin:1rem;
-      
-        padding:0.3rem;
-        color:gold;
-        font-family: 'Courier New', Courier, monospace;
-        font-style: italic;
-        margin:3px;
-    }
-    #boton1{
-        width: 35%;
-    }
-    #boton2{
-        width: 65%;
-    }
-
-</style>
-
-<body>
 <div class="form-insert">
 
     <?php if(isset($sesiones) && count($sesiones)>0):?>
-        <h2>Sesiones encontradas:</h2>
-        <table>
+       
+        <table id="table-sesion" class="table-sesion">
             <thead>
+                  <h2 class="titulos-sesion">Sesiones encontradas:</h2>
                 <tr>
                     <th>No.</th>
                     <th>Lugar</th>
                     <th>Entrenador</th>
                     <th>Fecha</th>
                     <th>Hora</th>
+                     <th>Acciones</th>
                 </tr>
             </thead>
             <tbody>
@@ -158,7 +46,8 @@
    <!-- Formulario para buscar sesiones programadas por lugar de entrenamiento -->
    <?php $user_email = isset($_GET['user_email']) ? $_GET['user_email'] : null; ?>
                 
-        <form action="index.php? action=list_sessionBySite" method="post" style="width: 96%;" class="form-insert" id="formulario">
+        <form action="index.php? action=list_sessionBySite" method="post"  id="form-sesion">
+             <h2 class="titulos-sesion">Elige un escenario</h2>
             <input type="hidden" name="user_email" id="user_email_input"> 
             <select name="id_lugar">
             
@@ -174,19 +63,104 @@
                         }
                     ?>
                 </select>
-                <input type="submit" value="Buscar Sesiones" class="form-botones">
+                <input type="submit" value="Buscar Sesiones" class="form-botones-sesion">
         </form>
-        <div style='display:flex; flex-direction: row; width:50%;' >
-            <form action='index.php?action=principal' method='post' id="boton1">
-                <button type='submit' name='action' value='principal'  class="boton-sub">Ir al inicio</button>
-            </form> 
-            <form action='index.php?action=trainer_manage' method='get' id="boton2">
-                <button type='submit' name='action' value='trainer_manage' class="boton-sub">Sesiones de Entrenamiento</button>
-            </form>
-        </div>
 </div> 
+<!-- ============================================================================================== -->
+<style>
+    @font-face {
+  font-family: 'fuente3';
+  src: url('fonts/fuente3.ttf') format('truetype');/*Nombre real : struck */
+}
+    .form-insert{
+        background-image: url('./IMG/LTL/mat.jpg');
+            /* background-size: cover; Ajusta la imagen para cubrir toda la pantalla */
+            background-position: center center; /* Centra la imagen */
+            background-attachment: fixed; /* Hace que la imagen se quede fija al hacer scroll */
+            background-repeat: no-repeat;
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+    }
+    #form-sesion{  
+        width: 60%;
+        display:flex;
+        flex-direction: column;
+        align-items: center;
+        background-color:#4A0D0D;
+        border: solid gold 2px;
+        margin-bottom: 3%;
+    }
+    #table-sesion{
+        width: 80%;
+        background-color:#4A0D0D;
+        color: #D4AF37;
+        font-style: italic;
+        border: solid gold 2px;
+        margin-bottom: 3%;
+        padding:1%;
+    }
+    .titulos-sesion{
+        width: 80%;
+        font-family: 'fuente3';
+        font-size: 1.8rem;
+        font-weight: lighter;
+        color: gold;
+        font-style: italic;
+        letter-spacing: 0.3rem;
+        text-align: center;
+        margin-bottom: 2%;
+    }
+    input{
+        width: 80%;
+       font-size: 1.3vw;
+       font-style: italic;
+       padding:2%;
+    }
+    option{
+        font-size: 0.8vw;
+        font-style: italic;
+    }
+    select{
+        font-size: 1vw;
+        font-style:italic;
+        padding: 1%;
+        width: 40%;
+    }
+    .form-botones-sesion{
+        background-color: transparent; 
+        border:  solid gold 1px;
+        border-radius: 5px;
+        font-family:Arial, Helvetica, sans-serif;
+        font-style: italic;
+        font-size: 1vw;
+        font-weight: bold;
+        color:gold;
+        width:40%;
+        margin:2%;
+        }
+    td, th{
+        padding: 1%;
+        font-size:1vw;
+        border:solid gold 1px;
+    }
+    
+    table{    
+        width: 100%;
+        border: solid gold 1px;
+    }
+    #formulario{
+        margin:5%;
+    }
+    .lugar{
+        width: 40%;
+    }
+</style>
 
 <script>
+
     let userEmail = localStorage.getItem('UserEmail');
 
 // Si existe 'user_email' en localStorage, asignarlo al input oculto
